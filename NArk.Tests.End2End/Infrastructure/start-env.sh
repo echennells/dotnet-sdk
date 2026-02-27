7767c203d0d1ff1c39bf52c233d43ef58955b024
Andrew Camilleri <evilkukka@gmail.com>
2026-02-27 11:31:12 +0100

    fix: use $NIGIRI variable instead of bare nigiri command
    Lines 292-294 used `nigiri` directly instead of `$NIGIRI` (the path
    to the locally-built binary), causing "command not found" on CI.

--- NArk.Tests.End2End/Infrastructure/start-env.sh
+++ NArk.Tests.End2End/Infrastructure/start-env.sh
@@ -289,9 +289,9 @@ fi
 
 
 # this is technically already handled in nigiri start
-nigiri ark init  --password secret --server-url localhost:7070 --explorer http://chopsticks:3000
-nigiri faucet $(nigiri ark receive | jq -r ".onchain_address") 2
-nigiri ark redeem-notes -n $(nigiri arkd note --amount 100000000) --password secret
+$NIGIRI ark init  --password secret --server-url localhost:7070 --explorer http://chopsticks:3000
+$NIGIRI faucet $($NIGIRI ark receive | jq -r ".onchain_address") 2
+$NIGIRI ark redeem-notes -n $($NIGIRI arkd note --amount 100000000) --password secret
 
 # 7. Setup Fulmine wallet
 setup_fulmine_wallet
