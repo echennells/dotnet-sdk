695982f817768636ce43a6aebea24007cd94da22
Andrew Camilleri <evilkukka@gmail.com>
2026-03-20 17:45:59 +0100

    fix: pass IVtxoStorage to PostBatchVtxoPollingHandler in tests

--- NArk.Tests/VtxoPollingHandlerTests.cs
+++ NArk.Tests/VtxoPollingHandlerTests.cs
@@ -44,6 +44,7 @@ public class PostBatchVtxoPollingHandlerTests
         _handler = new PostBatchVtxoPollingHandler(
             vtxoSyncService,
             _contractStorage,
+            _vtxoStorage,
             _options);
     }
 
